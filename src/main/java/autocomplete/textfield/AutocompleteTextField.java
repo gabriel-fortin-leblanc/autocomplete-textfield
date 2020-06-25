008bec490fdb601654c524f6184706d5c735f748
Gabriel <gabriel.fortin-leblanc@umontreal.ca>
2020-06-25 14:38:47 -0400

    first commit

---
+++ src/main/java/autocomplete/textfield/AutocompleteTextField.java
@@ -0,0 +1,14 @@
+package autocomplete.textfield;
+
+import javafx.scene.control.TextField;
+
+/**
+ * This class reprensents an autocomplete textfield.
+ *
+ * @author Gabriel Fortin
+ */
+public class AutocompleteTextField extends TextField {
+
+
+
+}
